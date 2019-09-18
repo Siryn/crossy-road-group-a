13216f75687867adbcde08da26013c8f4ead8323
andrewiscool144 <55508319+andrewiscool144@users.noreply.github.com>
2019-09-18 16:15:01 -0400

    Basic UI Stuff
    I will do more with it when there is gameplay.

---
+++ crossyRoadGroupA/Assets/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject startPanel;
+    public GameObject creditsPanel;
+
+    private void Start()
+    {
+        startPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+
+    public void OnClickQuitButton()
+    {
+        Application.Quit();
+        print("PRINT BUTTON!!!!!!");
+    }
+    public void OnClickCreditsButton()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void OnClickBackButton()
+    {
+        creditsPanel.SetActive(false);
+    }
+    public void OnClickPlayButton()
+    {
+        startPanel.SetActive(false);
+    }
+}
