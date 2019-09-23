b5d801ec724920bb3dee105888bf8748d9959376
Lauren <47837659+Siryn@users.noreply.github.com>
2019-09-23 15:23:37 -0400

    Added high score script from lecture
    There's also a not-at-all completed PlayerMovement script that I'll be working on 9/25/19 and I adjusted the Main Camera to view the game as you would in CrossyRoad.

---
+++ crossyRoadGroupA/Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class HighScoreManager : MonoBehaviour
+{
+    public int[] highScores = new int[10];
+
+    private void Awake()
+    {
+        ReadHighScores();
+    }
+
+    public void OnGameEnd(int score)
+    {
+        SaveHighScores(score);
+    }
+
+    private SaveData CreateSaveDataObject()
+    {
+        SaveData save = new SaveData();
+        for (int i = 0; i < highScores.Length; i++)
+        {
+            save.highScores[i] = highScores[i];
+        }
+        return save;
+    }
+
+    public void SaveHighScores(int newScore)
+    {
+        highScores[0] = 22;
+        highScores[1] = 21;
+        highScores[2] = 15;
+        highScores[3] = 12;
+        highScores[4] = 11;
+        highScores[5] = 8;
+        highScores[6] = 3;
+        highScores[7] = newScore;
+        highScores[8] = 0;
+        highScores[9] = 0;
+
+        SaveData save = CreateSaveDataObject();
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gameSave.save");
+        bf.Serialize(file, save);
+        file.Close();
+        Debug.Log("Data saved with score " + newScore);
+    }
+
+    public void ReadHighScores()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gameSave.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameSave.save", FileMode.Open);
+            SaveData save = (SaveData)bf.Deserialize(file);
+            file.Close();
+
+            for (int i = 0; i < highScores.Length; i++)
+            {
+                highScores[i] = save.highScores[i];
+            }
+        }
+        Debug.Log("Date loaded");
+    }
+}
---
+++ crossyRoadGroupA/Assets/Scripts/SaveData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public int[] highScores;
+
+    public SaveData()
+    {
+        highScores = new int[10];
+    }
+}
