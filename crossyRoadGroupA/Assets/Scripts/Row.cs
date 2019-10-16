0a2d1f40189bd368665c22f2ba10c282b1da66e0
LMillz <lmillzdarxider@users.noreply.github.com>
2019-10-16 06:56:16 -0400

    Level Gen v2
    Not sure where I am supposed to be pushing this but when I sent it too the master and then pulled it to test again, everything broke. So here is a messier throw together of part of what I had. Will put together the generation on each row in class.

---
+++ crossyRoadGroupA/Assets/Scripts/Row.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Row
+{
+    ////////////
+    // VARIABLES
+    ////////////
+    public GameObject[] rowTiles = new GameObject[5];
+    private List<GameObject> instancedCell = new List<GameObject>();
+    ////////////
+
+    /////////////
+    // Properties
+    /////////////
+    public int rowType { get; set; }
+    public int genType { get; set; }
+    public Vector3 positionRow { get; set; }
+    /////////////
+
+    ///////////////
+    // CONSTRUCTORS
+    ///////////////
+    public Row(GameObject[] tiles, int row, int gen, Vector3 position)
+    {
+        rowTiles = tiles;
+        rowType = row;
+        genType = gen;
+        positionRow = position;
+
+        SpawnRow(rowType, genType, positionRow);
+    }
+    ///////////////
+
+    //////////
+    // METHODS
+    //////////
+    public void SpawnRow(int row, int gen, Vector3 position) //Adds the designated row
+    {
+        for (int i = 0; i < GlobalVariables.tileCells; i++)
+        {
+            instancedCell.Add(GameObject.Instantiate(rowTiles[row], (position + new Vector3(0, 0, i)), Quaternion.identity));
+        }
+    }
+
+    public void DestroyRow(int row) //Removes the designated row
+    {
+        for (int i = 0; i < instancedCell.Count; i++)
+        {
+            GameObject.Destroy(instancedCell[i]);
+        }
+    }
+    //////////
+}
